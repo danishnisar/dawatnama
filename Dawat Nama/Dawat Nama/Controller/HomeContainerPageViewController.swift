e83bb6c5ea25bf5785527fc859f9b4011417464b
Danish Nisar <danishnisar.aptechnn@gmail.com>
2018-07-16 20:17:53 +0500

    Pagination Version 1.0

--- Dawat Nama/Dawat Nama/Controller/HomeContainerPageViewController.swift
+++ Dawat Nama/Dawat Nama/Controller/HomeContainerPageViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class HomeContainerPageViewController: UIPageViewController,UIPageViewControllerDelegate,UIPageViewControllerDataSource {
+class HomeContainerPageViewController: UIPageViewController,UIPageViewControllerDelegate,UIPageViewControllerDataSource,UIScrollViewDelegate {
     
+    var mvcMain = MainViewController()
     
+    var baseIndex:Int = 0
 
-    lazy var storyB:[UIViewController] = {
+     lazy var storyB:[UIViewController] = {
         return [
             UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageHome_ViewController") as! PageHome_ViewController,
-            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageAlert_ViewController") as! PageAlert_ViewController
-         //   UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageProfile_ViewController") as! PageProfile_ViewController
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageAlert_ViewController") as! PageAlert_ViewController,
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageProfile_ViewController") as! PageProfile_ViewController
         ]
     }()
 
@@ -24,9 +26,11 @@ class HomeContainerPageViewController: UIPageViewController,UIPageViewController
         super.viewDidLoad()
         
         self.delegate = self
-        self.dataSource = self 
-        setViewControllers([storyB[0]], direction: .forward, animated: true, completion: nil)
-        regNotification()
+        self.dataSource = self
+        
+        funccall_i()
+        regNot()
+        
         
     }
 //    override func viewWillAppear(_ animated: Bool) {
@@ -34,25 +38,10 @@ class HomeContainerPageViewController: UIPageViewController,UIPageViewController
 //
 //    }
     
-    
-    func regNotification(){
-        NotificationCenter.default.addObserver(self, selector: #selector(self.checkoverif), name: NSNotification.Name("\(0)"), object: nil)
-    }
-    
-    @objc func checkoverif(){
-       print("a")
-
-        DispatchQueue.main.async {
-            
-            self.setViewControllers([self.storyB[0]], direction: .forward, animated: true, completion: nil)
-
-        }
-    
-        
-    }
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return storyB.count
     }
+    
     
     
     
@@ -61,24 +50,69 @@ class HomeContainerPageViewController: UIPageViewController,UIPageViewController
    
     
         let index = storyB.index(of: viewController) ?? 0
+  
         if index <= 0 {
+            baseIndex = index+1
             return nil
         }
+    
+        baseIndex = index-1
         return storyB[index-1]
         
         
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         let index  = storyB.index(of: viewController) ?? 0
+        //print(index)
+        
         if index >= storyB.count-1 {
+            baseIndex = index-1
             return nil
         }
+        //viewController.index
+        baseIndex = index+1
         return storyB[index+1]
     }
-   
+    
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        //mvcMain.baselineToggle(indexPath: baseIndex)
+        NotificationCenter.default.post(name: NSNotification.Name("\(baseIndex)"), object: nil)
+        print(baseIndex)
+        
+        
+    }
+    
+    
+    
+    func regNot(){
+        NotificationCenter.default.addObserver(self, selector: #selector(funccall_i), name: NSNotification.Name("\(0.0)"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(funccall_ii), name: NSNotification.Name("\(1.0)"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(funccall_iii), name: NSNotification.Name("\(2.0)"), object: nil)
+    }
+    
+    @objc func funccall_i(){
+        
+        setViewControllers([storyB[0]], direction: .forward, animated: true, completion: nil)
+    }
+    @objc func funccall_ii(){
+        
+       setViewControllers([storyB[1]], direction: .forward, animated: true, completion: nil)
+    }
+    @objc func funccall_iii(){
+        
+       setViewControllers([storyB[2]], direction: .forward, animated: true, completion: nil)
+    }
+    
   
     
+
+
     
+  
+    
+  
 
 }
--- Dawat Nama/Dawat Nama/Controller/MainViewController.swift
+++ Dawat Nama/Dawat Nama/Controller/MainViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class MainViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
    
-    
+    lazy var storyB:[UIViewController] = {
+        return [
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageHome_ViewController") as! PageHome_ViewController,
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageAlert_ViewController") as! PageAlert_ViewController,
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PageProfile_ViewController") as! PageProfile_ViewController
+        ]
+    }()
+
     // Mark :- IBOUTLETs Home View
     
     @IBOutlet weak var Menucollection: UICollectionView!
@@ -22,19 +29,38 @@ class MainViewController: UIViewController,UICollectionViewDelegate,UICollection
         return ["Screen-slicing_04","Screen-slicing_07","Screen-slicing_09"]
     }()
     var bottomLine:[UIView] = [UIView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         Menucollection.delegate = self
         Menucollection.dataSource = self
+    
+        
+
         // Do any additional setup after loading the view, typically from a nib.
         regMenucellview()
+        regNot()
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func regNot(){
+        NotificationCenter.default.addObserver(self, selector: #selector(funccall_i), name: NSNotification.Name("\(0)"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(funccall_ii), name: NSNotification.Name("\(1)"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(funccall_iii), name: NSNotification.Name("\(2)"), object: nil)
+    }
+    
+    @objc func funccall_i(){
+        
+        baselineToggle(indexPath: 0)
+    }
+    @objc func funccall_ii(){
+        
+        baselineToggle(indexPath: 1)
+    }
+    @objc func funccall_iii(){
+        
+        baselineToggle(indexPath: 2)
     }
     
     
@@ -62,6 +88,7 @@ class MainViewController: UIViewController,UICollectionViewDelegate,UICollection
         
         cell.menuicon.image = UIImage(named: menu[indexPath.item])
         bottomLine.append(cell.bottom)
+        //print("cell index path\(indexPath.item)")
         cell.bottom.isHidden = (indexPath.item == 0) ? false : true
         
         
@@ -85,27 +112,40 @@ class MainViewController: UIViewController,UICollectionViewDelegate,UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.item)
-        NotificationCenter.default.post(name: NSNotification.Name("\(indexPath.item)"), object: nil)
+        print("Float:\(Float(indexPath.item))")
         
-        for ind in 0...indexPath.count {
-            if ind == indexPath.item {
-                bottomLine[ind].isHidden = false
-            }else{
-                bottomLine[ind].isHidden = true
-            }
-        }
-    
+        NotificationCenter.default.post(name: NSNotification.Name("\(Float(indexPath.item))"), object: nil)
+        baselineToggle(indexPath:indexPath.item)
+        
     }
     
+    func baselineToggle(indexPath:Int){
+        
+        for ind in 0...2 {
+            if ind == indexPath {
+                print(ind)
+                UIView.animate(withDuration: 0.1) {
+                     self.bottomLine[ind].isHidden = false
+                    
+                }
+
+            }else{
+                UIView.animate(withDuration: 0.1) {
+                    self.bottomLine[ind].isHidden = true
+                }
+               
+            }
+
+        }
+        
+       
+
+    }
         //bottomLine[indexPath.item].isHidden = false
         //Menucollection.layoutIfNeeded()
     
     
-    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
-        
-       // Menucollection.selec
-    }
+    
     
 
     

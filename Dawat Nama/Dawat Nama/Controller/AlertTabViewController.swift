32fd93715dc339ce0602cb7979ed5421d7eb3744
Danish Nisar <danishnisar.aptechnn@gmail.com>
2018-07-17 22:52:01 +0500

    TabBar Cell AutoLayout And Fixing Of Tab Bar Menu

--- Dawat Nama/Dawat Nama/Controller/AlertTabViewController.swift
+++ Dawat Nama/Dawat Nama/Controller/AlertTabViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class AlertTabViewController: UIViewController,TabSwiper {
 
+    //MARK :- Added Variable Of IBOUTLET & IBACTION
+    
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +20,7 @@ class AlertTabViewController: UIViewController,TabSwiper {
 
             InitSwipView(direction: .left)
             InitSwipView(direction: .right)
+            CellReg()
         
     }
     func HandleSwipe(_ Sender: UISwipeGestureRecognizer) {
@@ -28,11 +32,39 @@ class AlertTabViewController: UIViewController,TabSwiper {
             
         }
     }
+    
+    func CellReg(){
+        tblView.register(UINib(nibName: "TblBaseCellView", bundle: nil), forCellReuseIdentifier: "cellBase")
+    }
 
    
 
 }
 
-extension AlertTabViewController:UITableViewController,UITableViewDelegate {
+extension AlertTabViewController:UITableViewDataSource,UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellbase = tableView.dequeueReusableCell(withIdentifier: "cellBase", for: indexPath) as! TblBaseCellView
+        
+        return cellbase
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
